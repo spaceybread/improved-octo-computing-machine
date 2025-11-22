9971f15ab9831987c166fb866ffeaa6c2de5f022
Soham Phargade <soham.g.phargade@gmail.com>
2025-11-22 13:53:54 -0600

    node and graph class

--- graph_network/Node.py
+++ graph_network/Node.py
@@ -1,17 +1,24 @@
 class Node: 
-    def __init__(self, v, el=None):
-        self.value = v
-        self.neighbors = el
+    def __init__(self, key):
+        self.key = key
+        self.neighbors = set() # set of Node objects
 
-    def set_value(self, v):
-        self.value = v
+    def add_neighbor(self, neighbor):
+        self.neighbors.add(neighbor)
+
+    def set_key(self, key):
+        self.key = key
 
     def get_neighbors(self):
         return self.neighbors
 
-    def set_neighbors(self, el): 
-        self.neighbors = el
+    def set_neighbors(self, neighbors):
+        self.neighbors = set(neighbors)
 
-    
+    def __hash__(self):
+        return hash(self.key)
+
+    def __eq__(self, other):
+        return isinstance(other, Node) and self.key == other.key
 
 
