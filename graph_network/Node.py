a717f2a0e38e40a5600dc0e74a71e647d9ed0f6d
Soham Phargade <soham.g.phargade@gmail.com>
2025-11-22 19:48:09 -0600

    decentralized node implementation

--- graph_network/Node.py
+++ graph_network/Node.py
@@ -1,24 +1,61 @@
-class Node: 
-    def __init__(self, key):
-        self.key = key
-        self.neighbors = set() # set of Node objects
+from collections import deque
 
-    def add_neighbor(self, neighbor):
-        self.neighbors.add(neighbor)
+class Node:
+    def __init__(self, MAC:str, neighbors=None):
+        self.MAC = MAC
+        self.neighbors = set()
+        self.cc = {self}
+        if neighbors:
+            self.add_node(neighbors)
+            
+    def __repr__(self):
+        return f"Node({self.MAC})"
 
-    def set_key(self, key):
-        self.key = key
+    def add_node(self, neighbors):
+        for n in neighbors:
+            self.neighbors.add(n)
+            n.neighbors.add(self)
+            self.cc |= n.cc
 
-    def get_neighbors(self):
-        return self.neighbors
-
-    def set_neighbors(self, neighbors):
-        self.neighbors = set(neighbors)
-
-    def __hash__(self):
-        return hash(self.key)
-
-    def __eq__(self, other):
-        return isinstance(other, Node) and self.key == other.key
+        visited = {self}
+        q = deque([self])
+        while q:
+            node = q.popleft()
+            visited.add(node)
+            node.cc = self.cc
+            for n in node.neighbors:
+                if n not in visited:
+                    q.append(n)
 
 
+    def remove_node(self):
+        nbrs = list(self.neighbors)
+        for n in nbrs:
+            n.neighbors.discard(self) # disconnect from neighbors
+        
+        self.neighbors = set()
+        self.cc = set()
+        visited = set() # recompute CC for each disconnected region
+
+        def recompute_cc(start): # helper
+            cc = set()
+            q = deque([start])
+            visited.add(start)
+            while q:
+                node = q.popleft()
+                cc.add(node)
+                for nbr in node.neighbors:
+                    if nbr not in visited:
+                        visited.add(nbr)
+                        q.append(nbr)
+            return cc
+
+        for nbr in nbrs:
+            if nbr not in visited:
+                comp = recompute_cc(nbr)
+                for node in comp:
+                    node.cc = comp
+
+if __name__ == "__main__":
+    n1 = Node("mac1")
+    n2 = Node("mac2", neighbors={n1})
